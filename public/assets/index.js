9421aee7fa863b2ba81a38a124aa4247ec003abe
Subashkumar S <subashs2232@gmail.com>
2024-07-17 22:23:50 +0530

    updated nextjs and typescript

---
+++ public/assets/index.js
@@ -0,0 +1,5 @@
+import hamburger from "./menu-outline.svg";
+import close from "./close.svg";
+import desktopHomeHeader from "./home/desktop/image-hero.jpg";
+
+export { hamburger, close, desktopHomeHeader };
