035fdea31c67f2b4358ce16d3d044ad3812b045a
Subashkumar S <subashs2232@gmail.com>
2024-07-17 22:20:26 +0530

    Delete public/assets directory

--- public/assets/index.js
+++
@@ -1,5 +0,0 @@
-import hamburger from "./menu-outline.svg";
-import close from "./close.svg";
-import desktopHomeHeader from "./home/desktop/image-hero.jpg";
-
-export { hamburger, close, desktopHomeHeader };
